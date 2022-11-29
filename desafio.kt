f7e8a625b5c5e0fc6c782815ed31d3f1c93474d8
Thomaz Massari <103146983+ThoMassari@users.noreply.github.com>
2022-11-29 10:29:59 -0300

    entregando projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Aluno(val nome: String, val matricula: String) {
+    override fun toString(): String {
+        return "$nome - matricula: $matricula"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "$nome: duracao $duracao horas"
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val inscritos = mutableListOf<Aluno>()
+
+    fun matricular(vararg alunos: Aluno) {
+        inscritos.addAll(alunos)
+    }
+
+    override fun toString(): String {
+        return """
+            Nome: $nome
+            Nivel: $nivel
+            Conteudo Educacional: $conteudos
+            Alunos matriculados: $inscritos
+            
+        """.trimIndent()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val thox = Aluno("Thox", "101010")
+    val dex = Aluno("Dex", "202020")
+  
+    val logicaDeProgrmacao = ConteudoEducacional("Logica de Programacao", 80)
+    val poo = ConteudoEducacional("POO", 80)
+    val java = ConteudoEducacional("Java")
+    val kotlin = ConteudoEducacional("Kotlin", 120)
+
+    val desenvolvedorMobile = Formacao(
+        "Desenvolvedor Mobile",
+        Nivel.BASICO,
+        listOf(logicaDeProgrmacao, poo, kotlin)
+    )
+
+    val desenvolvedorBackEnd = Formacao(
+        "Desenvolvedor BackEnd",
+        Nivel.INTERMEDIARIO,
+        listOf(logicaDeProgrmacao, poo, java)
+    )
+
+    desenvolvedorMobile.matricular(thox)
+    desenvolvedorBackEnd.matricular(dex)
+
+    println(desenvolvedorMobile)
+    println(desenvolvedorBackEnd)
 }
